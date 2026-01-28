13bc4ad2710f69944c2f872fa22b88b34f96ff8f
Mihai PLESA <mihai.plesa@gmail.com>
2026-01-28 15:40:22 +0000

    password complexity enforcement

--- philonet/settings.py
+++ philonet/settings.py
@@ -104,15 +104,25 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'OPTIONS': {
+            'user_attributes': ('username', 'email', 'first_name', 'last_name'),
+            'max_similarity': 0.7,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 12,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+    {
+        'NAME': 'questions.validators.ComplexityPasswordValidator',
     },
 ]
 
